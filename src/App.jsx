39d606dbf211f2e39f7016b7248f68b72ea4b049
carlos reyes <carlosarp2000@gmail.com>
2022-05-13 15:19:21 -0500

    creacion y configuracion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Las notas de Carlos</h1>
+      </header>
+      <Router>
+        <Routes>
+
+        </Routes>
+      </Router>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;
